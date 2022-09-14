53f58eb83e6fc1782d355740ee97ea10fe8785f7
hannah-deakin <hannah.v.deakin@gmail.com>
2022-09-14 12:57:21 +0100

    Added html link to App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ export default function App() {
       Weather App
       <footer>
         This app was coded by Hannah Deakin and is {""}
-        <a href="" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/hannah-deakin/hannah-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on GitHub
         </a>
       </footer>
