5fad0e6e65846cdec543ca8356168998219eed78
hannah-deakin <hannah.v.deakin@gmail.com>
2022-09-14 16:39:36 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This app was coded by Hannah Deakin and is {""}
-        <a
-          href="https://github.com/hannah-deakin/hannah-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This app was coded by Hannah Deakin and is {""}
+          <a
+            href="https://github.com/hannah-deakin/hannah-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
