83865fd9bdb751c7c973f6df6a1794d00f8c0258
hannah-deakin <hannah.v.deakin@gmail.com>
2022-09-14 12:59:52 +0100

    Added h1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <footer>
         This app was coded by Hannah Deakin and is {""}
         <a
