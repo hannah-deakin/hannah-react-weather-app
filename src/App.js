e3a51afa269a360cefe81a6854b86e9023fd3c5e
hannah-deakin <hannah.v.deakin@gmail.com>
2022-09-14 12:51:44 +0100

    Cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      Weather App
+      <footer>
+        This app was coded by Hannah Deakin and is {""}
+        <a href="" target="_blank" rel="noreferrer">
+          open-sourced on GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
