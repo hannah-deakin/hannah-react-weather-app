2b1212258fb49905523f630d3cf5e35d0cb63895
hannah-deakin <hannah.v.deakin@gmail.com>
2022-09-15 17:59:25 +0100

    Added API integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Cartagena" />
         <footer>
           <a
             href="https://github.com/hannah-deakin/hannah-react-weather-app"
--- src/Weather.js
+++ src/Weather.js
@@ -1,85 +1,92 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
-import SearchForm from "./SearchForm";
-import WeatherOverview from "./WeatherOverview.js";
-import TemperatureOverview from "./TemperatureOverview.js";
 
-export default function Weather() {
-  let [ready, setReady] = useState(false);
-  let [temperature, setTemperature] = useState(null);
+export default function Weather(props) {
+  let [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
     console.log(response.data);
-    setTemperature(Math.round(response.data.main.temp));
+    setWeatherData({
+      ready: true,
+      date: "Thursday 15:00",
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    });
   }
 
-  let city = "Cartagena";
-  let apiKey = "29d72df3f212ca58205d7001ac0bf3cf";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="Weather">
-      <div className="SearchForm">
-        <form className="mb-3">
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Search a city..."
-                className="form-control"
-                autoComplete="off"
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
-        </form>
-      </div>
-      //
-      <div className="WeatherOverview">
-        <div className="overview">
-          <h1>{weatherData.city}</h1>
-          <ul>
-            <li>
-              Last updated: {weatherData.date}, {weatherData.time}
-            </li>
-            <li>{weatherData.description}</li>
-          </ul>
-        </div>
-      </div>
-      //
-      <div className="TemperatureOverview">
-        <div className="row">
-          <div className="col-6">
-            <div className="d-flex weather-temperature">
-              <img
-                src={weatherData.imgUrl}
-                alt={weatherData.description}
-                className="float-left"
-              />
-              <div className="float-left">
-                <strong>{weatherData.temperature}</strong>
-                <span className="units">
-                  {" "}
-                  <a href="/">°C</a> | <a href="/">°F</a>
-                </span>
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="SearchForm">
+          <form className="mb-3">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Search a city..."
+                  className="form-control"
+                  autoComplete="off"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-100"
+                />
               </div>
             </div>
-          </div>
-          <div className="col-6">
+          </form>
+        </div>
+
+        <div className="WeatherOverview">
+          <div className="overview">
+            <h1>{weatherData.city}</h1>
             <ul>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind} m/h</li>
+              <li>Last updated: {weatherData.date}</li>
+              <li>{weatherData.description}</li>
             </ul>
           </div>
         </div>
+
+        <div className="TemperatureOverview">
+          <div className="row">
+            <div className="col-6">
+              <div className="d-flex weather-temperature">
+                <img
+                  src={weatherData.iconUrl}
+                  alt={weatherData.description}
+                  className="float-left"
+                />
+                <div className="float-left">
+                  <strong>{Math.round(weatherData.temperature)}</strong>
+                  <span className="units">
+                    {" "}
+                    <a href="/">°C</a> | <a href="/">°F</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind}m/h</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "29d72df3f212ca58205d7001ac0bf3cf";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading... ";
+  }
 }
