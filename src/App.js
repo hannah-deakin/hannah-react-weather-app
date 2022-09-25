1d1065b0ed580e258607c85338d8c7006dbea743
hannah-deakin <hannah.v.deakin@gmail.com>
2022-09-25 11:13:40 +0100

    Added link and styling to App footer credentials

--- src/App.js
+++ src/App.js
@@ -7,14 +7,30 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Cartagena" />
         <footer>
+          Code by{" "}
+          <a
+            href="https://github.com/hannah-deakin"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Hannah Deakin
+          </a>
+          , open-source on{" "}
           <a
             href="https://github.com/hannah-deakin/hannah-react-weather-app"
             target="_blank"
             rel="noreferrer"
           >
-            Open-sourced code
+            GitHub
           </a>
-          {""} by Hannah Deakin
+          , and hosted on{" "}
+          <a
+            href="https://peppy-pithivier-6f681b.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
         </footer>
       </div>
     </div>
